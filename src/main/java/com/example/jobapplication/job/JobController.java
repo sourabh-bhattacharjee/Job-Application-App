6346cbae0499716e5ae5e0d0fddf54ab19e6926a
sourabh bhattacharjee <sourabh20051998@gmail.com>
2026-06-15 12:32:51 +0200

    mongoDb Integration in  jobapplication

--- src/main/java/com/example/jobapplication/job/JobController.java
+++ src/main/java/com/example/jobapplication/job/JobController.java
@@ -1,4 +1,58 @@
 package com.example.jobapplication.job;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/jobs")
 public class JobController {
+
+    private JobService jobService;
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job){
+        jobService.createJob(job);
+        return new ResponseEntity<>("Jobs Created Successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable String id){
+        Job job = jobService.getJobById(id);
+        if(job!= null){
+            return new ResponseEntity<>(job,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable String id) {
+        boolean delete = jobService.deleteJob(id);
+        if(delete){
+            return new ResponseEntity<>("Job deleted successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("job with id not found", HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable String id, @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id,updatedJob);
+        if(updated) return new ResponseEntity<>("Successfully Updated",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
 }
--- src/main/java/com/example/jobapplication/job/impl/JobServiceImpl.java
+++ src/main/java/com/example/jobapplication/job/impl/JobServiceImpl.java
@@ -1,4 +1,73 @@
 package com.example.jobapplication.job.impl;
 
-public class JobServiceImpl {
+import com.example.jobapplication.job.Job;
+import com.example.jobapplication.job.JobRepository;
+import com.example.jobapplication.job.JobService;
+import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class JobServiceImpl implements JobService {
+
+    private JobRepository jobRepository;
+
+
+
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job getJobById(String id) {
+        List<Job> jobs = jobRepository.findAll();
+        for(Job job:jobs){
+            if(job.getId().equals(id)){
+                return job;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean deleteJob(String id) {
+        if(jobRepository.existsById(id)){
+            jobRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(String id, Job updatedJob) {
+
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if(jobOptional.isPresent()){
+
+            Job job = jobOptional.get();
+
+            job.setLocation(updatedJob.getLocation());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+
+    }
 }
