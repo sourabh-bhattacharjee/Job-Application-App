9b6056c209f408bc54cfab10ae4b69fc36368226
sourabh bhattacharjee <sourabh20051998@gmail.com>
2026-06-21 19:52:02 +0200

    enabling inter service communication using rest template and eureka server

--- companyMicroservice/src/main/java/com/example/companymicroservice/company/client/ReviewClient.java
+++ companyMicroservice/src/main/java/com/example/companymicroservice/company/client/ReviewClient.java
@@ -1,18 +1,14 @@
 package com.example.companymicroservice.company.client;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
-@Service
-public class ReviewClient {
-    private final RestClient restClient;
-    public ReviewClient(@LoadBalanced RestClient.Builder builder){
-        this.restClient = builder.baseUrl("""
-                http://REVIEWMICROSERVICE""").build();
-    }
-    public void deleteAllByCompanyId(String id) {
-        restClient.delete().uri("/reviews/{id}/deleteAll").retrieve().toBodilessEntity();
-    }
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="REVIEWMICROSERVICE")
+public interface ReviewClient {
+
+    @DeleteMapping("/reviews/{id}/deleteAll")
+    void deleteAllByCompanyId(@PathVariable String id);
 }
